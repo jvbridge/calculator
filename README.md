db133e2a5df8ad95cbfc2b37625be24ce2411485
jvbridge <jvbridge@gmail.com>
2022-01-11 17:21:44 -0800

    Got update of calc to work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This is just a quick way for me to teach myself the basics of NODE and serving
 static folders
+
+learning with the help of random googling and:
+https://www.section.io/engineering-education/building-a-calculator-a-javascript-project-for-beginners/
--- calc.js
+++ calc.js
@@ -1,13 +1,67 @@
+// get a list of all the number buttons
+const numberButtons = document.querySelectorAll("[data-number]");
+// get a list of all the operators
+const operationButtons = document.querySelectorAll("[data-operation]");
+// special buttons
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+// previous value
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+// current value
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+
+// make listeners for all the number buttons
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+/**
+ * Main Class for the calculator
+ */
 class Calculator {
-  constructor() {
-    this.currOp = null;
-    this.prevOp = null;
-    this.display = "";
+  // sets up the elements we need to update and remember
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.currOpElement = currentOperandTextElement;
+    this.prevOpElement = previousOperandTextElement;
+    this.clear();
   }
-  clear() {}
-  del() {}
+
+  clear() {
+    this.currOp = "";
+    this.prevOp = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
   appendNumber(num) {}
   choose(operation) {}
   eval() {}
-  updateDisplay() {}
+  updateDisplay() {
+    this.currOpElement.innerText = this.getDisplayNumber(this.currOp);
+    if (this.operation != null) {
+      this.prevOpElement.innerText = `${this.getDisplayNumber(this.prevOp)} ${
+        this.operation
+      }`;
+    } else {
+      this.prevOpElement.innerText = "";
+    }
+  }
+  getDisplayNumber(number) {
+    const floatNumber = parseFloat(number);
+    if (isNaN(floatNumber)) return "";
+    return floatNumber.toLocaleString("en");
+  }
 }
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
